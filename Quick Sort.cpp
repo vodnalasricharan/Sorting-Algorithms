3b67bd5f72ce3555b344339a6795617e612d9a92
Sricharan <vodnalasricharan@gmail.com>
2021-07-10 14:08:01 +0530

    #10072021
    https://www.youtube.com/watch?v=3Bbm3Prd5Fo&list=PL2_aWCzGMAwKedT2KfDMB9YA5DgASZb3U&index=7
    https://www.youtube.com/watch?v=3Bbm3Prd5Fo&list=PL2_aWCzGMAwKedT2KfDMB9YA5DgASZb3U&index=8

---
+++ Quick Sort.cpp
@@ -0,0 +1,66 @@
+// Swapping two values.
+void swap(int *a, int *b)
+{
+	int temp; 
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+ 
+// Partitioning the array on the basis of values at high as pivot value.
+int Partition(int a[], int low, int high)
+{
+	int pivot, index, i;
+	index = low;
+	pivot = high;
+ 
+	// Getting index of the pivot.
+	for(i=low; i < high; i++)
+	{
+		if(a[i] < a[pivot])
+		{
+			swap(&a[i], &a[index]);
+			index++;
+		}
+	}
+	// Swapping value at high and at the index obtained.
+	swap(&a[pivot], &a[index]);
+ 
+	return index;
+}
+ 
+// Random selection of pivot.
+int RandomPivotPartition(int a[], int low, int high)
+{
+	int pvt, n, temp;
+	n = rand();
+	// Randomizing the pivot value in the given subpart of array.
+	pvt = low + n%(high-low+1);
+ 
+	// Swapping pivot value from high, so pivot value will be taken as a pivot while partitioning.
+	swap(&a[high], &a[pvt]);
+ 
+	return Partition(a, low, high);
+}
+ 
+int QuickSort(int a[], int low, int high)
+{
+	int pindex;
+	if(low < high)
+	{
+		// Partitioning array using randomized pivot.
+		pindex = RandomPivotPartition(a, low, high);
+		// Recursively implementing QuickSort.
+		QuickSort(a, low, pindex-1);
+		QuickSort(a, pindex+1, high);
+	}
+	return 0;
+}
+
+
+
+int main() {
+    int A[]={1,4,2,7,4,6,1,3,0,9,4};
+    QuickSort(A,0,10);
+    for(int i=0;i<11;i++)cout<<A[i]<<" ";
+}
